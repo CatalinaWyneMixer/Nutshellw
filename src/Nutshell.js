1c3d257cb0d954b29d58b93ceaaa76d8003bc1e9
Akeen11 <aartkeen@ut.utm.edu>
2018-09-04 11:37:18 -0500

    moved shit around

--- src/Nutshell.js
+++ src/Nutshell.js
@@ -1,2 +1,20 @@
-import App from './App';
+import React, { Component } from "react"
+import NavBar from "./nav/NavBar"
+import ApplicationViews from "./ApplicationViews"
 
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./Nutshell.css"
+
+
+class Nutshell extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <NavBar />
+                <ApplicationViews />
+            </React.Fragment>
+        )
+    }
+}
+
+export default Nutshell
