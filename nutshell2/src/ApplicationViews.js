83f18c293c222c762cbcda6cc31720175ee8e201
Akeen11 <aartkeen@ut.utm.edu>
2018-09-04 10:19:53 -0500

    fun stuff

--- nutshell2/src/ApplicationViews.js
+++ nutshell2/src/ApplicationViews.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-// import Friend from './components/Freind'
+import NewsForm from './Forms/NewsForm'
+// import Friend from './components/Friend'
 // import Login from './components/Login'
 // import Task from './components/Task'
 // import Message from './components/Message'
@@ -12,14 +11,16 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      <div>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Nutshell Dos!</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+      </div>
+      <NewsForm />
       </div>
     );
   }
