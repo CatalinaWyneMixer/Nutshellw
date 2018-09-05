5dbc211bbb2deaf70cade29617824d32cf7d5617
Jessteus <mateusvanhalen@gmail.com>
2018-09-05 12:17:47 -0500

    events lunch

---
+++ src/components/EventDetails.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react"
+import "./Event.css"
+import events_pic from "./events_pic.jpg"
+
+
+
+export default class EventDetail extends Component {
+    render() {
+        /*
+            Using the route parameter, find the animal that the
+            user clicked on by looking at the `this.props.animals`
+            collection that was passed down from ApplicationViews
+        */
+        const event = this.props.events.find(a => a.id === parseInt(this.props.match.params.eventId)) || {}
+
+        return (
+            <section className="event">
+                <div key={event.id} className="detail-card">
+                    <div className="card-body">
+                        <h4 className="card-title">
+                            <img src={events_pic} className="icon--event" />
+                            {event.date}
+                        </h4>
+                        <h6 className="card-title">{event.name}</h6>
+                        <h6 className="card-title">{event.details}</h6>
+                        <a href="#"
+                            onClick={() => this.props.deleteEvent(event.id)
+                                            .then(() => this.props.history.push("/events"))}
+                            className="card-link">Delete</a>
+                            <a href="#"
+                            onClick={() => this.props.history.push(`/events/edit/${event.id}`)}
+                            className="card-link">Edit</a>
+                    </div>
+                </div>
+            </section>
+        )
+    }
+}
